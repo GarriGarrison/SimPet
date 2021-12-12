9404b3102b02e9f3cf6b94ed30ce68e9a0600ef1
Анастасия Хлыстова <90859233+AppleSan27@users.noreply.github.com>
2021-12-12 20:39:18 +0300

    Delete client directory

--- client/src/redux/state.js
+++
@@ -1,7 +0,0 @@
-// state
-
-// const initialState = [] // если в стейте лежит что-то одно, то делаем только один редьюсер и сразу его передаем в стор
-
-export const initialState = {
-  user:null
-}
